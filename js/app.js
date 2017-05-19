c453a0f4cce261876159091a498c629524526d76
Henry <henry32144@hotmail.com>
2017-05-19 18:19:12 +0800

    Implement Enemy's movement

--- js/app.js
+++ js/app.js
@@ -2,10 +2,12 @@
 var Enemy = function() {
     // 要应用到每个敌人的实例的变量写在这里
     // 我们已经提供了一个来帮助你实现更多
-
+    this.column = Math.round((Math.random()*10)/4);
+    this.speed = Math.round(Math.random()*1000);
     // 敌人的图片或者雪碧图，用一个我们提供的工具函数来轻松的加载文件
     this.x = 0;
-    this.y = 60;
+    this.y = 60 + (this.column*83);
+
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -14,7 +16,9 @@ var Enemy = function() {
 Enemy.prototype.update = function(dt) {
     // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
     // 都是以同样的速度运行的
+    this.x += this.speed * dt;
 };
+//Enemy.prototype.checkCollision 
 
 // 此为游戏必须的函数，用来在屏幕上画出敌人，
 Enemy.prototype.render = function() {
